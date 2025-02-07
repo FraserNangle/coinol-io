b969a92dee69a5e53272f691afdd493defd22d85
Fraser Nangle <fraser.m.nangle@gmail.com>
2025-02-07 20:05:16 +0000

    package upgrades and prebuild with deleted webp files

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = ["babel-preset-expo"];
-  const plugins = ["react-native-reanimated/plugin"];
+  const plugins = [];
 
   return {
     presets,
